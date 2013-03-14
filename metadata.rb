8e74c0f4ec4aeb92a939cc2f239188885f66cfe5
Joey Aghion <joey@aghion.com>
2013-03-14 13:05:20 -0400

    Don't declare rails::configure recipe. It causes librarian-chef to fail on dependency resolution.

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,6 @@ version "0.1"
 recipe "custom_env::configure", "Write a config/application.yml file to app's deploy directory. Relies on restart command declared by rails::configure recipe. (Intended as part of configure/deploy OpsWorks events.)"
 recipe "custom_env::update", "Write an updated config/application.yml and restart the app. Can be run independently of OpsWorks configure/deploy events."
 
-depends "rails::configure"
+# This actually depends on the rails::configure recipe by OpsWorks, but not
+# declaring that here to prevent librarian-chef failure.
+# depends "rails::configure"
