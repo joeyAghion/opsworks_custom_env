d18533b047bd4fd0e6b5d312f2db5acd025c774a
Joey Aghion <joey@aghion.com>
2013-08-13 18:08:39 -0400

    Now that the restart command must be provided outside of this cookbook, configure and update recipes can be consolidated.

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -1,10 +1,6 @@
 # Set up app's custom configuration in the environment.
 # See https://forums.aws.amazon.com/thread.jspa?threadID=118107
 
-# Experiment with not explicitly including rails::configure, to make it easier
-# to adapt this cookbook to other layers, like rails-worker.
-# include_recipe "rails::configure"
-
 node[:deploy].each do |application, deploy|
   
   custom_env_template do
