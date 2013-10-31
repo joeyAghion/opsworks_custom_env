ac06eab09f6f6b5601af1401ec68a1611f09eb6a
Joey Aghion <joey@aghion.com>
2013-10-31 00:59:06 -0400

    Try an alternative recipe structure. Always define a cookbook-specific command for restarting rails app.

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,7 @@
 maintainer "Artsy"
 description "Writes a config/application.yml file with custom ENV values to apps' deploy directories."
-version "0.2"
+version "0.3"
 
-recipe "opsworks_custom_env::deploy", "Write custom configuration and notify rails application to restart (depends on rails::configure recipe, so useful in deploy event)."
-recipe "opsworks_custom_env::update", "Write custom configuration and restart rails app with own command (doesn't depend on rails::configure recipe, so useful for ad-hoc configuration changes)."
-recipe "opsworks_custom_env::write_config", "Write a config/application.yml file to app's deploy directory. Used by deploy and update recipes, or can be used directly in custom situations."
-
-# We depend on the rails::configure recipe by OpsWorks, but commenting this out to avoid
-# librarian-chef errors.
-# depends "rails::configure"
+recipe "opsworks_custom_env::configure", "Write custom configuration and notify rails application to restart upon changes."
+recipe "opsworks_custom_env::restart_command", "Helper recipe that defines command to restart rails application."
+recipe "opsworks_custom_env::write_config", "Write a config/application.yml file to app's deploy directory. Used by configure recipe, or can be used directly in custom situations."
--- recipes/deploy.rb
+++
@@ -1,5 +0,0 @@
-# During deploy event, a 'restart Rails app <name>' command is already defined, so
-# we simply include that recipe and notify the existing "resource."
-
-include_recipe "rails::configure"
-include_recipe "opsworks_custom_env::write_config"
